480ecc85a73d7c95ff9dbd7f5bc7e6ff19c6f9ca
Asami <90178926+Asamijonsson@users.noreply.github.com>
2025-06-08 22:57:42 +0200

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,17 @@
+import Header from "@/components/Header";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <main className="home-container">
+      <Header className="sticky left-0 top-0">
+        <div className="flex items-center gap-2 lg:gap-4">
+          Notification
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
+    </main>
+  );
+}
---
+++ components/CollaborativeRoom.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/Header";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+import { ClientSideSuspense, RoomProvider } from "@liveblocks/react";
+
+const CollaborativeRoom = () => {
+  return (
+    <RoomProvider id="my-room">
+      <ClientSideSuspense fallback={<div>Loading…</div>}>
+        <div className="collaborative-room">
+          <Header>
+            <div className="flex w-fit items-center justify-center gap-2">
+              <p className="document-title">This is a fake document title</p>
+            </div>
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </Header>
+          <Editor />
+        </div>
+      </ClientSideSuspense>
+    </RoomProvider>
+  );
+};
+
+export default CollaborativeRoom;
